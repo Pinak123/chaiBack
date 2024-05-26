d4fb7eafe612df13480f333da1c72f805e2836b7
Pinak123 <77087801+Pinak123@users.noreply.github.com>
2024-05-26 22:42:38 +0530

    setup middlewares

--- src/index.js
+++ src/index.js
@@ -3,7 +3,15 @@ import dotenv from "dotenv";
 import mongoose from "mongoose";
 import { DB_NAME } from "./constants.js";
 import connect_DB from "./db/index.js";
-
+import app from "./app.js";
 dotenv.config({ path: "./env" });
 
-connect_DB();
+connect_DB()
+.then(()=> {
+    app.listen(process.env.PORT || 3000 , ()=>{
+        console.log("Server listening on port " + process.env.PORT);
+    });
+})
+.catch(err => {
+
+});
