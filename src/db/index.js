cd430e22c70049b9bace6c7d7dd07f049ee4d3dc
Pinak123 <77087801+Pinak123@users.noreply.github.com>
2024-05-26 22:28:37 +0530

    connected to mongo atlas

--- src/db/index.js
+++ src/db/index.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 import { DB_NAME } from "../constants.js";
 import dotenv from "dotenv";
 dotenv.config();
+
 const MONGODB_URI1 = "mongodb+srv://pinakss7:SAMARTHA@cluster0.vcojfb2.mongodb.net?retryWrites=true&w=majority&appName=Cluster0"
 const connect_DB = async () => {
   try {
@@ -10,7 +11,7 @@ const connect_DB = async () => {
       `${process.env.MONGODB_URI}/${DB_NAME}`
     );
   
-    console.log("Mongodb connected");
+    console.log("Mongodb connected " + connectionInstance.connection.host);
   } catch (e) {
     console.log("Mongodb connection error", e);
 
