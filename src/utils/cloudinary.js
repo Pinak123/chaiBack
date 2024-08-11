df426408df2b32fce04310924d4004e94255af2b
Pinak123 <77087801+Pinak123@users.noreply.github.com>
2024-08-12 01:40:48 +0530

    did some work regarding cloudiinary

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,25 @@
+import {v2 as cloudinary} from 'cloudinary';
+import fs from 'fs';
+
+cloudinary.config({ 
+        cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+        api_key: process.env.CLOUDINARY_API_KEY, 
+        api_secret: process.env.CLOUDINARY_API_SECRET // Click 'View Credentials' below to copy your API secret
+    });
+
+ const uploadOnCloudinary = async (localFilePath) =>{
+    try {
+        if (!localFilePath) return null;
+        ///Upload file
+        const response = await cloudinary.uploader.upload(localFilePath , {
+            resource_type:'auto',
+        })
+        //File loaded successfully
+        console.log("The file is uploaded on cloudinary " , response.url);
+
+    } catch (error) {
+        fs.unlinkSync(localFilePath); //Removed temporary saved file
+        return null; //
+        
+    }
+ }
